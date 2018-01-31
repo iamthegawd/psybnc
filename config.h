b65b3e84101d28ad6bbadce961b12d80f20c4d0b
root <root@localhost.localdomain>
2018-01-31 09:56:27 -0600

    first commit

---
+++ config.h
@@ -0,0 +1,55 @@
+/*
+ * Configuration file for psyBNC, created by menuconf
+ */
+
+/* Encryption */
+
+#define CRYPT
+
+/* Encryption Type*/
+
+#define BLOWFISH
+
+/* Allow traffic logging */
+
+//#define TRAFFICLOG
+
+/* Allow the dcc File-Functions */
+
+#define DCCFILES
+
+/* Pipe dcc Chats */
+
+#define DCCCHAT
+
+/* Allow to add more users */
+
+#define MULTIUSER
+
+/* Number of max. Users */
+
+#define MAXUSER 500
+
+/* Number of max. Connections per User */
+
+#define MAXCONN 3
+
+/* Allow the usage of scripts */
+
+#define SCRIPTING
+
+/* Support oIdentd */
+
+#define OIDENTD
+
+/* Allow multiple irc connections per user */
+
+#define NETWORK
+
+/* The logging level */
+
+#define LOGLEVEL 0
+
+/* SSL-Security */
+
+#define SSLSEC 0
